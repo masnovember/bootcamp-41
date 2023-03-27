e252dcefe760c65bf337bc497262d455de0bd84c
miguelsolis <masnoviembre@gmail.com>
2023-03-26 20:03:55 -0500

    Implement ms_service_discovery

---
+++ bank-registry-service/src/main/java/com/nttdata/bank/registry/service/BankRegistryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.nttdata.bank.registry.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class BankRegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankRegistryServiceApplication.class, args);
+	}
+
+}
---
+++ bank-registry-service/src/test/java/com/nttdata/bank/registry/service/BankRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bank.registry.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
